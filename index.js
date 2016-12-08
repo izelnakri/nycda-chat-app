4a9d79184194a4b0e737f317412c58d2fc4010d6
Izel Nakri <contact@izelnakri.com>
2016-12-08 12:10:56 +0100

    very basic Facebook chat is working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express'),
       hbs = require('hbs'),
       request = require('request'),
       compression = require('compression'),
+      session = require('express-session'),
       APP_PORT = process.env.PORT || 3005;
 
 let app = express(),
@@ -17,33 +18,41 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(compression());
 
+app.use(session({
+  secret: 'keyboard cat'
+}));
+
 app.use(express.static('public', {
   maxAge: '1y'
 }));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  console.log(req.session);
+  if (req.session.user) {
+    res.render('application', { user: req.session.user });
+  } else {
+    res.render('welcome');
+  }
 });
 
 app.get('/fb-login', (req, res) => {
-  request(`https://graph.facebook.com/v2.8/oauth/access_token?client_id=1829373514000205&redirect_uri=localhost:3005/fb-token&code=${req.query.code}`, (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-    } else {
+  request(`https://graph.facebook.com/v2.8/oauth/access_token?client_id=1829373514000205&redirect_uri=http://localhost:3005/fb-login&code=${req.query.code}&client_secret=7af34a02aaf09fb7f1882b019afbf0e3`, (error, response, body) => {
+    var jsonResponse = JSON.parse(body),
+        url = `https://graph.facebook.com/me?access_token=${jsonResponse.access_token}&fields=email,name,first_name,last_name`;
+
+    request(url, (error, response, body) => {
+      var userData = JSON.parse(body);
+      req.session.user = userData;
       res.redirect('/');
-    }
+    });
   });
+});
 
-
+app.get('/logout', (req, res) => {
+  req.session.user = undefined;
   res.redirect('/');
 });
 
-
-app.get('/fb-token', (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-  res.redirect('/');
-});
 
 var server = app.listen(APP_PORT, () => {
   console.log(`Web server started on port ${APP_PORT}`);
